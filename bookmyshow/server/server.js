0ee5ce5130cd656d25461cf6574823e9b9b3e936
ArunRao1997 <rao.nayineni1997@gmail.com>
2023-08-29 11:43:49 -0400

    'add-movie' api

--- bookmyshow/server/server.js
+++ bookmyshow/server/server.js
@@ -8,9 +8,12 @@ require('dotenv').config()
 const dbConfig = require('./config/dbconfig')
 
 const userRoute = require('./routes/userRoutes')
+const movieRoute = require('./routes/movieRoute')
 
 app.use(express.json())
 app.use('/api/users', userRoute)
+app.use('/api/users', movieRoute)
+
 app.listen(8082, ()=>{
     console.log('Server is up and running')
 })
